54689fc325f7202b15c2956aea51d38ee7907810
jagadheesh2003 <jagadheeshpkj@gmail.com>
2024-07-20 14:03:20 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,22 +1,38 @@
-from flask import Flask,request,render_template
+import numpy as np
+from flask import Flask, request, jsonify, render_template
 import pickle
-app= Flask(__name__)
-@app.route('/')
-def hello_world():
-    return render_template("login.html")
-database={'jagadheesh':'123','jeevaprasath':'234','dhanush':'432'}
+app = Flask(__name__)
+model = pickle.load(open('linear','rb'))
 
-@app.route('/form_login',methods=['POST','GET'])
-def login():
-    name1=request.form['username']
-    pwd=request.form['password']
-    if name1 not in database:
-        return render_template('login.html',info='Invalid User')
-    else:
-         if database[name1]!=pwd:
-             return render_template('login.html',info='Invalid password')
-         else:
-             return render_template('home.html',name=name1)
-         
-if __name__=='__main__':
-    app.run()
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model. predict(final_features)
+    val=int(prediction[0])
+
+    #output  =  round(prediction[0],3)
+
+
+    return render_template('index.html',prediction_text='Estimated car price in Euro{}'.format(val))
+
+
+#@app.route('/predict_api',methods=['POST])
+# def predict_api():
+#     '''
+#      For direct API calls trought request
+#      '''
+#      data = request.get_json(force=true)
+#      prediction = model.predict([np.array(list(data.values()))])
+
+
+
+#      output = prediction[0]
+#      return jsonify(output)
+
+if __name__ == "__main__":
+   app.run(debug=True) 
